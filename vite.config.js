e57f64550b5bb382094e50bfbc64ae5a74f838e5
Minh Truong <minhtruongdevvn@gmail.com>
2024-04-20 12:25:56 +0930

    🔧 build(config): fix vite config

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,12 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/note.css',
             ],
             refresh: true,
-        }),
+        })
     ],
+    build: {
+        outDir: 'public/build', // Customize the output directory
+    }
 });
